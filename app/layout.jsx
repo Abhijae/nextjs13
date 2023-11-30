0c8910ce9df53a7ca9b58389c2cef26931e21ac5
Abhijae <abhijae@gmail.com>
2023-11-30 22:17:26 +0530

    api call define for the tickets component

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,8 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Navbar/>
+      <body className={rubik.className}>
         {children}
       
       </body>
